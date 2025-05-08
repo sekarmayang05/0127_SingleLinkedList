cbccb45b9d55d56491b4b0bb80a86fefbb47a741
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-08 16:09:30 +0700

    menggunakan tippe data boolean

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,4 @@
- while ((current != NULL) && (nim>= current->noMhs))
-    {
-        if (nim == current-> noMhs)
-        {
-            cout << "\nDuplikasi noMhs tidak dijinkan\n";
-            return;
-        }
-        prevoius = current;
-        current = current->next;
-
-    }
+bool lisEmpty()
+{
+    return (START == NULL);
+}
