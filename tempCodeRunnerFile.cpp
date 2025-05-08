1bf2873f590e89b323d4bae579c330ee7ffe0605
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-08 17:10:29 +0700

    menabhakan input case 3

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,6 @@
-case '2':
+break;
+        case '3':
         {
-            if (mhs.lisEmpty())
-            {
-                cout << endl
-                    << "List Kosong" << endl;
-                break;
-            }
-            cout << endl
-                << "\nMasukkan no mahasiswa yang akan dihapus : ";
-            cin >> nim;
-            if (mhs.delNode(nim)== false)
-                cout << endl
-                    << "Data tidak ditemukan" << endl;
-            else
-                cout << endl
-                    << "Data dengan npmor mahasiswa " << nim << "berhasil dihapus " << endl;
+            mhs.traverse();
         }
         break;
