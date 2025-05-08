4935b168e22cfc7b0ce992273a3486ff34b95cdc
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-08 17:11:11 +0700

    menambahakan case 4 dan menggunakan perulanagn if

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,23 @@
-break;
-        case '3':
+case '4':
         {
-            mhs.traverse();
+            if (mhs.lisEmpty() == true)
+            {
+                cout <<"\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout <<endl
+                << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                    << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                    << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
         }
         break;
