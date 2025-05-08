9cd3191fc8f6890338c23c217eda8ae596b95076
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-08 17:09:23 +0700

    menambahkan input case 2

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,19 @@
-
-int main()
-{
-    List mhs;
-    int nim;
-    char ch;
-    while (1)
-    {
+case '2':
+        {
+            if (mhs.lisEmpty())
+            {
+                cout << endl
+                    << "List Kosong" << endl;
+                break;
+            }
+            cout << endl
+                << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim)== false)
+                cout << endl
+                    << "Data tidak ditemukan" << endl;
+            else
+                cout << endl
+                    << "Data dengan npmor mahasiswa " << nim << "berhasil dihapus " << endl;
+        }
+        break;
