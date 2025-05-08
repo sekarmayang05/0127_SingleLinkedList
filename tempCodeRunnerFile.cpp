92d0977f5decad18f8e1112ab991bced1de27e52
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-08 17:16:38 +0700

    implementasikan menu keluar pada case 5

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,23 +1,13 @@
-case '4':
+case '5':
         {
-            if (mhs.lisEmpty() == true)
-            {
-                cout <<"\nList Kosong\n";
-                break;
-            }
-            Node *previous, *current;
-            cout <<endl
-                << "Masukkan no mahasiswa yang dicari : ";
-            cin >> nim;
-            if (mhs.Search(nim, &previous, &current) == false)
-                cout << endl
-                    << "Data tidak ditemukan" << endl;
-            else
-            {
-                cout << endl
-                    << "Data ditemukan" << endl;
-                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
-                cout << "\n";
-            }
         }
         break;
+        deafult:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
+        }
+    } while (ch != '5');
+
+}
